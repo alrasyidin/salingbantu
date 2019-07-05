de5f67ce6b7450ddbc591430aa37149b50ba9961
hellodit <asditap@gmail.com>
2019-07-05 10:59:26 +0700

    Create auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('avatar');
-            $table->string('identity_photo');
+            $table->uuid('id')->primary();
             $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('username');
+            $table->string('last_name');
+            $table->text('bio')->nullable();
+            $table->integer('verified')->default(0);
+            $table->string('avatar')->default('avatar.png');
+            $table->string('identity_photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
