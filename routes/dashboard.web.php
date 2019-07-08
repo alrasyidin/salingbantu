90e7c6baff14873515191722139037494cabb51d
hellodit <asditap@gmail.com>
2019-07-08 15:03:39 +0700

    layouts admin

--- routes/dashboard.web.php
+++ routes/dashboard.web.php
@@ -1,2 +1,6 @@
 <?php
 
+Route::group(['prefix' => 'dashboard','namespace' => 'Dashboard'], function () {
+    Route::get('/', 'HomeDashboardController@index');
+    // Route::view('/', 'dashboard.index');
+});
