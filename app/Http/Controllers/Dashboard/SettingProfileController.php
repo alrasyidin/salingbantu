cefb6c7ea6ed26d779310fdaedcd964ebb6877a6
hellodit <asditap@gmail.com>
2019-08-16 18:20:01 +0700

    [Asdita] - Dashboard #Setting Page

--- app/Http/Controllers/Dashboard/SettingProfileController.php
+++ app/Http/Controllers/Dashboard/SettingProfileController.php
@@ -15,7 +15,7 @@ class SettingProfileController extends Controller
 {
     public function index(){
         $modal = true;
-        return view('dashboard.setting-account.index',compact('modal'));
+        return view('app.dashboard.setting',compact('modal'));
     }
 
     public function store(Request $request){
@@ -27,7 +27,7 @@ class SettingProfileController extends Controller
             "bio"        => "min:50|max:1000",
             "email"      => "required|max:30|unique:users,email,$current_id",
         ]);
-        
+
 
         if($image=$request->file('user_photo')){
             $current_avatar = User::where('id', \Auth::user()->id)->first()->avatar;
@@ -38,13 +38,13 @@ class SettingProfileController extends Controller
             if (!File::isDirectory($this->path)) {
                 File::makeDirectory($this->path, 0777, true);
             }
-            
+
             if(file_exists(public_path("storage/user_photo/".$current_avatar))) {
                 Storage::delete("public/user_photo/".$current_avatar);
                 Storage::delete("public/user_photo/300/".$current_avatar);
                 Storage::delete("public/user_photo/60/".$current_avatar);
             }
-            
+
             $imgg = str_replace("[removed]", "", $request->input('user_photo2'));
             $imgg = str_replace('data:image/png;base64,', '', $imgg);
             $imgg = str_replace('data:image/jpeg;base64,', '', $imgg);
@@ -53,17 +53,17 @@ class SettingProfileController extends Controller
 
             $imageName   = \Auth::user()->username.'-'.uniqid().'.'.$image->getClientOriginalExtension();
             Image::make($crop_img)->save($this->path . '/' . $imageName);
-                
+
             foreach ($this->dimensions as $row) {
                     $canvas = Image::canvas($row, $row);
                     $resizeImage  = Image::make($crop_img)->resize($row, $row, function($constraint) {
                         $constraint->aspectRatio();
                     });
-                    
+
                     if (!File::isDirectory($this->path . '/' . $row)) {
                         File::makeDirectory($this->path . '/' . $row);
                     }
-    
+
                     $canvas->insert($resizeImage, 'center');
                     $canvas->save($this->path . '/' . $row . '/' . $imageName);
             }
@@ -79,7 +79,7 @@ class SettingProfileController extends Controller
                 'email' => $request->email,
                 'bio' => $request->bio
         ]);
-        
+
 
         return redirect('dashboard/setting')->with('success','Berhasil Memperbarui Profile');
     }
